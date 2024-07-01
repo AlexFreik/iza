951a4162d7c59a3c04e5c2b17e3f33cadfefbcc8
AlexFreik <alexfreik5@gmail.com>
2024-07-01 13:27:53 +0530

    fix bug: forget to update user after batch set

--- src/main.js
+++ src/main.js
@@ -207,6 +207,7 @@ function submitBatchClick() {
         if (error) {
             renderBatchSelectionPage(user, sessions, error);
         }
+        user.batch = batch;
         return renderSessionsPage(user, sessions, error);
     });
 }
@@ -307,6 +308,43 @@ function renderBatchSelectionPage(user, sessions, error) {
             able to change the batch later.
           </p>
         </div>
+
+        <div class="overflow-x-auto max-w-96">
+          <table class="table">
+            <!-- head -->
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Favorite Color</th>
+              </tr>
+            </thead>
+            <tbody>
+              <!-- row 1 -->
+              <tr class="bg-base-200">
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+              </tr>
+              <!-- row 2 -->
+              <tr>
+                <th>2</th>
+                <td>Hart Hagerty</td>
+                <td>Desktop Support Technician</td>
+                <td>Purple</td>
+              </tr>
+              <!-- row 3 -->
+              <tr>
+                <th>3</th>
+                <td>Brice Swyre</td>
+                <td>Tax Accountant</td>
+                <td>Red</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>`);
 }
 
@@ -354,6 +392,7 @@ function getSessionHtml(user, session) {
 }
 
 function renderSessionsPage(user, sessions, error) {
+    console.log(JSON.stringify(user), sessions, error);
     setNavbarRight(`
         <li class="w-36 text-center m-auto">${user.name}</li>
         <li><button onclick="signOutClick();" class="btn btn-outline btn-accent btn-sm">Sign Out</button></li>`);
