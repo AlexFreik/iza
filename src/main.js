0273acda054aecf042bfc325a465335a7a5f052b
AlexFreik <alexfreik5@gmail.com>
2024-07-01 12:15:28 +0530

    refactoring and basic sessions ui

--- src/main.js
+++ src/main.js
@@ -13,7 +13,37 @@ class User {
     }
 }
 
+class Session {
+    constructor(data) {
+        console.assert(data.length === 6);
+
+        this.lang = data[0];
+        this.date = data[1];
+        this.name = data[3];
+        this.batch = data[4];
+        this.time = new Date(data[5]);
+
+        this.start = combineDateAndTime(this.date, this.time);
+    }
+}
+
 // ===== Utils =====
+function combineDateAndTime(date, time) {
+    const ans = new Date(date);
+    ans.setHours(time.getHours(), time.getMinutes());
+    return ans;
+}
+
+function toISTString(date) {
+    return new Date(date).toLocaleString('en-US', {
+        month: 'short',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'Asia/Kolkata',
+    });
+}
+
 function setLoginError(msg) {
     const errorDiv = document.getElementById('email-not-found');
     errorDiv.innerHTML = msg;
@@ -29,75 +59,80 @@ function setNavbarRight(htmlStr) {
     elem.innerHTML = htmlStr;
 }
 
-function fetchUserAndRerender(email) {
-    renderLoadingPage();
-
+function fetchUser(email, f) {
     google.script.run
         .withFailureHandler((e) => {
-            renderLoginPage(email, "[Internal Error] Couldn't fetch details: " + e.toString());
+            return f(null, "[Internal Error] Couldn't fetch details: " + e.toString());
         })
         .withSuccessHandler((resStr) => {
             let res = {};
             try {
                 res = JSON.parse(resStr);
             } catch (e) {
-                return renderLoginPage(
-                    email,
-                    "[Internal Error] Couldn't parse response: " + e.toString(),
-                );
+                return f(null, "[Internal Error] Couldn't parse response: " + e.toString());
             }
 
             if (res.error === 'USER_NOT_FOUND') {
-                return renderLoginPage(
-                    email,
+                return f(
+                    null,
                     `[Error] There are no registrations for this email.
-                      Please check the spelling or use a different email 🙏`,
+                            Please check the spelling or use a different email 🙏`,
                 );
             } else if (res.error) {
-                return renderLoginPage(email, '[Error]: ' + res.error);
+                return f(null, '[Error]: ' + res.error);
             }
-            const user = new User(res.row, res.data);
-            currentUser = user;
-            if (user.batch === '') {
-                return renderBatchSelectionPage(user, '');
-            }
-            return renderUserPage(user);
+            return f(new User(res.row, res.data), null);
         })
         .getUserByEmail(email);
 }
 
-function setUserBatchAndRerender(user, batch) {
+function setUserBatch(user, batch, f) {
     google.script.run
         .withFailureHandler((e) => {
-            renderBatchSelectionPage(
-                user,
-                "[Internal Error] Couldn't select time batch: " + e.toString(),
-            );
+            return f("[Internal Error] Couldn't select time batch: " + e.toString());
         })
         .withSuccessHandler((resStr) => {
             let res = {};
             try {
                 res = JSON.parse(resStr);
             } catch (e) {
-                return renderBatchSelectionPage(
-                    user,
-                    "[Internal Error] Couldn't parse response: " + e.toString(),
-                );
+                return f("[Internal Error] Couldn't parse response: " + e.toString());
             }
 
             if (res.error === 'BATCH_NOT_FOUND') {
-                return renderBatchSelectionPage(
-                    user,
-                    `[Client Error] Batch ${batch} doesn't exist 🙏`,
-                );
+                return f(`[Client Error] Batch ${batch} doesn't exist 🙏`);
+            } else if (res.error) {
+                return f('[Error] ' + res.error);
             }
-            if (res.error) {
-                return renderBatchSelectionPage(user, '[Error] ' + res.error);
-            }
-            console.log(user);
-            return renderUserPage(user);
+
+            return f(null);
         })
         .setUserBatch(user.row, batch);
+}
+
+function fetchSessions(f) {
+    google.script.run
+        .withFailureHandler((e) => {
+            return f(null, "[Internal Error] Couldn't fetch sessions: " + e.toString());
+        })
+        .withSuccessHandler((resStr) => {
+            let res = {};
+            try {
+                res = JSON.parse(resStr);
+            } catch (e) {
+                return f(null, "[Internal Error] Couldn't parse response: " + e.toString());
+            }
+
+            if (res.error) {
+                return f(null, '[Error] ' + res.error);
+            }
+
+            const sessions = res.sessions
+                .map((row) => new Session(row))
+                .sort((a, b) => a.start.getTime() - b.start.getTime());
+            return f(sessions, null);
+        })
+        .getSessions();
 }
 
 // ===== Actions =====
@@ -107,23 +142,52 @@ function logInClick() {
         return renderLoginPage('', '[Error] Please enter valid email 🙏');
     }
     localStorage.setItem('email', email);
-    fetchUserAndRerender(email);
+
+    renderLoadingPage();
+    fetchUser(email, (user, error) => {
+        if (error) {
+            return renderLoginPage(email, error);
+        }
+        console.assert(user);
+        currentUser = user;
+        fetchSessions((sessions, error) => {
+            if (error) {
+                return renderLoginPage(email, error);
+            }
+            console.assert(sessions);
+            currentSessions = sessions;
+            if (user.batch === '') {
+                return renderBatchSelectionPage(user, sessions, error);
+            }
+            return renderSessionsPage(user, sessions, error);
+        });
+    });
 }
 
 function signOutClick() {
     localStorage.setItem('email', '');
     currentUser = null;
-    renderLoginPage('', '');
+    renderLoginPage('', null);
 }
 
 function submitBatchClick() {
+    const user = currentUser;
+    const sessions = currentSessions;
+
     const radio = document.querySelector('input[name="batch-radio"]:checked');
     if (radio === null) {
-        return renderBatchSelectionPage(currentUser, '[Error] Please select the batch 🙏');
+        return renderBatchSelectionPage(user, sessions, '[Error] Please select the batch 🙏');
     }
     const batch = radio.value;
     console.assert(batch === 'Morning' || batch === 'Evening');
-    setUserBatchAndRerender(currentUser, batch);
+
+    renderLoadingPage();
+    setUserBatch(user, batch, (error) => {
+        if (error) {
+            renderBatchSelectionPage(user, sessions, error);
+        }
+        return renderSessionsPage(user, sessions, error);
+    });
 }
 
 // ===== Page Rendering =====
@@ -141,12 +205,12 @@ function renderLoadingPage() {
     `);
 }
 
-function renderLoginPage(email, loginError) {
+function renderLoginPage(email, error) {
     setNavbarRight('');
 
     setRoot(`
       <div class="w-fit m-auto">
-        <div class="card card-compact bg-base-100 m-10 max-w-96 shadow-xl">
+        <div class="card card-compact bg-neutral-content m-10 max-w-96 shadow-xl">
           <figure>
             <img
               src="${IECO_COVER_IMG}"
@@ -155,7 +219,7 @@ function renderLoginPage(email, loginError) {
           <div class="card-body">
             <h2 class="card-title">Inner Engeneering Online</h2>
 
-            <label class="input input-bordered flex items-center gap-2">
+            <label class="input input-bordered bg-neutral-content flex items-center gap-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 16 16"
@@ -169,9 +233,9 @@ function renderLoginPage(email, loginError) {
               <input id="email-input" type="email" class="grow" placeholder="Email" value="${email}" />
             </label>
 
-            <div id="email-not-found" class="text-error">${loginError}</div>
+            <div id="email-not-found" class="text-error">${error ? error : ''}</div>
             <div class="card-actions justify-end">
-              <button onclick="logInClick();" class="btn btn-accent font-bold text-neutral-content">
+              <button onclick="logInClick();" class="btn btn-accent font-bold text-white">
                   Log In
               </button>
             </div>
@@ -180,7 +244,7 @@ function renderLoginPage(email, loginError) {
       </div>`);
 }
 
-function renderBatchSelectionPage(user, error) {
+function renderBatchSelectionPage(user, sessions, error) {
     setNavbarRight(`
       <li class="w-36 text-center m-auto">${user.name}</li>
       <li><button onclick="signOutClick();" class="btn btn-outline btn-accent btn-sm">Sign Out</button></li>`);
@@ -213,8 +277,8 @@ function renderBatchSelectionPage(user, error) {
             </label>
           </div>
 
-          <button class="btn btn-accent font-bold text-neutral-content mt-5" onclick="submitBatchClick();">Submit</button>
-          <p class="text-error">${error}</p>
+          <button class="btn btn-accent font-bold text-white mt-5" onclick="submitBatchClick();">Submit</button>
+          <p class="text-error">${error ? error : ''}</p>
 
           <p class="mt-5 text-left">
             <strong>Note:</strong> By selecting the batch time,
@@ -225,32 +289,57 @@ function renderBatchSelectionPage(user, error) {
       </div>`);
 }
 
-function renderUserPage(user) {
+function getSessionHtml(user, session) {
+    return `
+        <div class="bg-neutral-content shadow-lg text-center rounded-box my-5 p-3">
+            <div class="font-semibold">${session.name} (${session.lang})</div>
+
+            <div>
+                Starts ${toISTString(session.start)}
+            </div>
+
+            <div>
+                <button
+                    class="btn btn-accent font-bold text-white mt-3"
+                    onclick="submitBatchClick();">
+                    Join
+                </button>
+            </div>
+        </div>
+    `;
+}
+
+function renderSessionsPage(user, sessions, error) {
     setNavbarRight(`
         <li class="w-36 text-center m-auto">${user.name}</li>
         <li><button onclick="signOutClick();" class="btn btn-outline btn-accent btn-sm">Sign Out</button></li>`);
-    setRoot(`
 
+    const userSessions = sessions.filter((s) => s.lang === user.lang && s.batch === user.batch);
+
+    setRoot(`
+        <div class="m-fit m-auto max-w-96">
+            <div class="m-10">
+                <p class="text-xl font-bold">Sessions</p>
+                ${userSessions.map((s) => getSessionHtml(user, s)).join('')}
+            </div>
+        </div>
         `);
 }
 
 function renderPage() {
     const email = localStorage.getItem('email');
     if (email) {
-        renderLoginPage(email, '');
+        renderLoginPage(email, null);
     } else {
-        renderLoginPage('', '');
+        renderLoginPage('', null);
     }
 }
 
 // ===== Code Execution =====
-let base = './';
-
-if (location.hostname !== 'localhost') {
-    base = 'https://alexfreik.github.io/iza/src/';
-}
-
-const IECO_COVER_IMG = base + 'ieo-cover.jpg';
+const BASE = location.hostname === 'localhost' ? './' : 'https://alexfreik.github.io/iza/src/';
+const IECO_COVER_IMG = BASE + 'ieo-cover.jpg';
 
 let currentUser = null;
+let currentSessions = null;
+
 renderPage();
--- src/test-utils.js
+++ src/test-utils.js
@@ -64,6 +64,118 @@ function __setUserBatch(row, batch) {
     return JSON.stringify({});
 }
 
+function __getSessions() {
+    return JSON.stringify({
+        sessions: [
+            ['Language', 'Date', 'Day of Week', 'Step', 'Batch', 'Time'],
+            [
+                'English',
+                '2024-07-06T18:30:00.000Z',
+                'Sunday',
+                'Orientation',
+                'Morning',
+                '1899-12-30T00:38:50.000Z',
+            ],
+            [
+                'English',
+                '2024-07-06T18:30:00.000Z',
+                'Sunday',
+                'Orientation',
+                'Evening',
+                '1899-12-30T12:38:50.000Z',
+            ],
+            [
+                'English',
+                '2024-07-07T18:30:00.000Z',
+                'Monday',
+                'Step 1',
+                'Morning',
+                '1899-12-30T00:38:50.000Z',
+            ],
+            [
+                'English',
+                '2024-07-07T18:30:00.000Z',
+                'Monday',
+                'Step 1',
+                'Evening',
+                '1899-12-30T12:38:50.000Z',
+            ],
+            [
+                'English',
+                '2024-07-08T18:30:00.000Z',
+                'Tuesday',
+                'Step 2',
+                'Morning',
+                '1899-12-30T00:38:50.000Z',
+            ],
+            [
+                'English',
+                '2024-07-08T18:30:00.000Z',
+                'Tuesday',
+                'Step 2',
+                'Evening',
+                '1899-12-30T12:38:50.000Z',
+            ],
+            [
+                'English',
+                '2024-07-09T18:30:00.000Z',
+                'Wednesday',
+                'Step 3',
+                'Morning',
+                '1899-12-30T00:38:50.000Z',
+            ],
+            [
+                'Hindi',
+                '2024-07-06T18:30:00.000Z',
+                'Sunday',
+                'Orientation',
+                'Morning',
+                '1899-12-30T00:38:50.000Z',
+            ],
+            [
+                'Hindi',
+                '2024-07-06T18:30:00.000Z',
+                'Sunday',
+                'Orientation',
+                'Evening',
+                '1899-12-30T12:38:50.000Z',
+            ],
+            [
+                'Hindi',
+                '2024-07-07T18:30:00.000Z',
+                'Monday',
+                'Step 1',
+                'Morning',
+                '1899-12-30T00:38:50.000Z',
+            ],
+            [
+                'Hindi',
+                '2024-07-07T18:30:00.000Z',
+                'Monday',
+                'Step 1',
+                'Evening',
+                '1899-12-30T12:38:50.000Z',
+            ],
+            [
+                'Hindi',
+                '2024-07-08T18:30:00.000Z',
+                'Tuesday',
+                'Step 2',
+                'Morning',
+                '1899-12-30T00:38:50.000Z',
+            ],
+            [
+                'Hindi',
+                '2024-07-08T18:30:00.000Z',
+                'Tuesday',
+                'Step 2',
+                'Evening',
+                '1899-12-30T12:38:50.000Z',
+            ],
+        ],
+    });
+}
+
 const google = {};
 google.script = {};
 google.script.run = {};
@@ -83,6 +195,16 @@ google.script.run.withFailureHandler = (errorF) => ({
             let res = null;
             try {
                 res = __setUserBatch(row, batch);
+            } catch (e) {
+                errorF(e);
+                return null;
+            }
+            f(res);
+        },
+        getSessions: () => {
+            let res = null;
+            try {
+                res = __getSessions();
             } catch (e) {
                 errorF(e);
                 return null;
